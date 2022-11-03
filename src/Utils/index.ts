f0003088d43e40acefa592b55091d868731ac47b
Ben Sewell <ben@calc-i.com>
2022-11-03 10:20:14 +0000

    Build

--- src/Utils/index.ts
+++ src/Utils/index.ts
@@ -1,5 +1,3 @@
 import { Platform } from 'react-native';
 
-export const setTestId = (id: string) => {
-	return Platform.OS === 'android' ? { accessibilityLabel: id } : { testID: id };
-};
+export const setTestId = (id: string) => (Platform.OS === 'android' ? { accessibilityLabel: id } : { testID: id });
